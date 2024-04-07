e3dd837af89c61646cfee6ab20be93cdea14c97a
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-04-07 20:14:05 +0530

    props implemented for cards

--- 01-Inception/app2.js
+++ 01-Inception/app2.js
@@ -1,6 +1,555 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const resList = [
+  {
+    info: {
+      id: "243870",
+      name: "Kritunga Jagavi's Restaurant",
+      cloudinaryImageId: "ebwsw7chqyxaqrgnuwav",
+      locality: "Raidurgam",
+      areaName: "Gachibowli",
+      costForTwo: "₹500 for two",
+      cuisines: [
+        "Biryani",
+        "Andhra",
+        "Rayalaseema",
+        "South Indian",
+        "North Indian",
+        "Chinese",
+      ],
+      avgRating: 3.9,
+      parentId: "473730",
+      avgRatingString: "3.9",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 25,
+        lastMileTravel: 3.5,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "3.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-04-07 22:45:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Rxawards/_CATEGORY-Coastal.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Coastal.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          logo: "Swiggy%20One%20Lite/One_lite_vertical_logo.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+        badgeType: "BADGE_TYPE_ONE_LITE",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/kritunga-jagavis-restaurant-raidurgam-gachibowli-hyderabad-243870",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "155268",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Kondapur",
+      areaName: "Kondapur",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 3.7,
+      parentId: "721",
+      avgRatingString: "3.7",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 32,
+        lastMileTravel: 2.8,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "2.8 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-04-07 23:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹179",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          logo: "Swiggy%20One%20Lite/One_lite_vertical_logo.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+        badgeType: "BADGE_TYPE_ONE_LITE",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/pizza-hut-kondapur-hyderabad-155268",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "344923",
+      name: "Baskin Robbins - Ice Cream Desserts",
+      cloudinaryImageId: "85ccae4e3576f9330af102c46ca85395",
+      locality: "Sri Ram Nagar, Kondapur",
+      areaName: "Sri Ram Nagar",
+      costForTwo: "₹250 for two",
+      cuisines: ["Desserts"],
+      avgRating: 4.4,
+      veg: true,
+      parentId: "5588",
+      avgRatingString: "4.4",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 21,
+        lastMileTravel: 2.4,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "2.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-04-08 05:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Rxawards/_CATEGORY-Ice-creams.png",
+            description: "Delivery!",
+          },
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Ice-creams.png",
+                },
+              },
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "40% OFF",
+        subHeader: "UPTO ₹80",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          logo: "Swiggy%20One%20Lite/One_lite_vertical_logo.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+        badgeType: "BADGE_TYPE_ONE_LITE",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/baskin-robbins-ice-cream-desserts-kondapur-sri-ram-nagar-hyderabad-344923",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "345899",
+      name: "Theobroma",
+      cloudinaryImageId: "63dd75492c47fcec191132b8eb299ea5",
+      locality: "Gachibowli",
+      areaName: "Gachibowli",
+      costForTwo: "₹400 for two",
+      cuisines: ["Desserts"],
+      avgRating: 4.5,
+      parentId: "1040",
+      avgRatingString: "4.5",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 21,
+        lastMileTravel: 2.9,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "2.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-04-07 23:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "20% OFF",
+        discountTag: "FLAT DEAL",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          logo: "Swiggy%20One%20Lite/One_lite_vertical_logo.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+        badgeType: "BADGE_TYPE_ONE_LITE",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/theobroma-gachibowli-hyderabad-345899",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "6398",
+      name: "Subway",
+      cloudinaryImageId: "63178e3e64d503a479f2a2048a474552",
+      locality: "SLN Terminus Gachibowli",
+      areaName: "Gachibowli",
+      costForTwo: "₹350 for two",
+      cuisines: ["Salads", "Snacks", "Desserts", "Beverages"],
+      avgRating: 4.3,
+      parentId: "2",
+      avgRatingString: "4.3",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 37,
+        lastMileTravel: 1.5,
+        serviceability: "SERVICEABLE",
+        slaString: "35-40 mins",
+        lastMileTravelString: "1.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-04-08 05:30:00",
+        opened: true,
+      },
+      badges: {
+        textExtendedBadges: [
+          {
+            iconId: "guiltfree/GF_Logo_android_3x",
+            shortDescription: "options available",
+            fontColor: "#7E808C",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "",
+                  fontColor: "#7E808C",
+                  iconId: "guiltfree/GF_Logo_android_3x",
+                  shortDescription: "options available",
+                },
+              },
+            ],
+          },
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹75 OFF",
+        subHeader: "ABOVE ₹499",
+        discountTag: "FLAT DEAL",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          logo: "Swiggy%20One%20Lite/One_lite_vertical_logo.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+        badgeType: "BADGE_TYPE_ONE_LITE",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/subway-sln-terminus-gachibowli-hyderabad-6398",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "15499",
+      name: "Burger King",
+      cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+      locality: "Hitech City",
+      areaName: "Madhapur",
+      costForTwo: "₹350 for two",
+      cuisines: ["Burgers", "American"],
+      avgRating: 4.1,
+      parentId: "166",
+      avgRatingString: "4.1",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 23,
+        lastMileTravel: 2.2,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "2.2 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-04-08 03:45:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Rxawards/_CATEGORY-Burger.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Burger.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "60% OFF",
+        subHeader: "UPTO ₹120",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          logo: "Swiggy%20One%20Lite/One_lite_vertical_logo.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+        badgeType: "BADGE_TYPE_ONE_LITE",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/burger-king-hitech-city-madhapur-hyderabad-15499",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "17096",
+      name: "KFC",
+      cloudinaryImageId: "f01666ac73626461d7455d9c24005cd4",
+      locality: "Patrika Nagar",
+      areaName: "Hitec City",
+      costForTwo: "₹400 for two",
+      cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+      avgRating: 3.9,
+      parentId: "547",
+      avgRatingString: "3.9",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 20,
+        lastMileTravel: 2.5,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "2.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-04-08 02:45:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Rxawards/_CATEGORY-Burger.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Burger.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "40% OFF",
+        subHeader: "UPTO ₹80",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        logoCtx: {
+          logo: "Swiggy%20One%20Lite/One_lite_vertical_logo.png",
+        },
+        freedelMessage: "FREE DELIVERY",
+        badgeType: "BADGE_TYPE_ONE_LITE",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/kfc-patrika-nagar-hitec-city-hyderabad-17096",
+      type: "WEBLINK",
+    },
+  },
+];
+
 function Header() {
   return (
     <div className="header">
@@ -22,18 +571,20 @@ function Header() {
   );
 }
 
-function ResCard() {
+function ResCard({ resData }) {
+  const { name, cuisines, avgRating, sla, cloudinaryImageId } = resData?.info;
+
   return (
     <div className="res-card">
       <img
         className="res-logo"
-        src="https://t3.ftcdn.net/jpg/01/96/80/24/360_F_196802485_VQxk0qmyPGTq56rKYXGikVGApD3A7v5T.jpg"
+        src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`}
         alt="res-logo"
       />
-      <h3>Meghana Foods</h3>
-      <h4>Biryani, North Indian</h4>
-      <h4>4.5 stars</h4>
-      <h4>38 mins</h4>
+      <h3>{name}</h3>
+      <h4>{`${cuisines[0]}${cuisines[1]}`}</h4>
+      <h4>{avgRating}</h4>
+      <h3>{`${sla.deliveryTime} mins`}</h3>
     </div>
   );
 }
@@ -43,17 +594,9 @@ function Body() {
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
+        {resList.map((res) => {
+          return <ResCard resData={res} />;
+        })}
       </div>
     </div>
   );
