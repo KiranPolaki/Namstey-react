64339c8fdc70421840759b9b7018f1bc085527d2
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-04-08 13:11:30 +0530

    05 - lets get hooked completed

--- 01-Inception/src/components/Body.js
+++ 01-Inception/src/components/Body.js
@@ -1,14 +1,28 @@
 import { ResCard } from "./ResCard.js";
 import { resList } from "../utils/mockData.js";
+import { useState } from "react";
 
 let index = 0;
 
 export function Body() {
+  const [listOfResto, setListOfResto] = useState(resList);
+
+  function applyFilter() {
+    const filteredList = resList.filter((res) => {
+      return res.info.avgRating >= 4;
+    });
+    setListOfResto(filteredList);
+  }
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button className="filter-btn" onClick={applyFilter}>
+          Top Rated restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((res) => {
+        {listOfResto.map((res) => {
           return <ResCard resData={res} key={index++} />;
         })}
       </div>
